a50772652471a5b20add2b2cfd49e5f0fc0b387d
AntonBaglaev <abaglaev@yandex.ru>
2022-09-17 22:40:00 +0300

    first commit

---
+++ config/params.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'admin_email' => 'abaglaev@yandex.ru',
+    'shop_name' => 'Магазин ishop2.loc',
+    'pagination' => 3,
+    'smpt_login' => '',
+    'smpt_password' => '',
+];
