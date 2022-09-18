62fe2ec2abd37c014010994fedc9629ede5053fd
AntonBaglaev <abaglaev@yandex.ru>
2022-09-18 18:45:32 +0300

    мелки правки

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -5,6 +5,10 @@ namespace app\controllers;
 
 class MainController{
 
+    public function __construct($route){
+        debug($route);
+    }
+
     public function indexAction(){
         echo __METHOD__;
     }
