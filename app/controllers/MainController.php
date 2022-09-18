8b7fab047f1a6f2f6d231a9cf830fcb5e1e85f3e
AntonBaglaev <abaglaev@yandex.ru>
2022-09-18 19:59:16 +0300

    20.00

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -2,15 +2,13 @@
 
 namespace app\controllers;
 
+use ishop\App;
 
-class MainController{
-
-    public function __construct($route){
-        debug($route);
-    }
+class MainController extends AppController{
 
     public function indexAction(){
         echo __METHOD__;
+        
     }
 
 }
--- vendor/ishop/core/base/Controller.php
+++ vendor/ishop/core/base/Controller.php
@@ -13,7 +13,21 @@ abstract class Controller{
     public $meta = [];
 
     public function __construct($route){
-        
+        $this->route = $route;
+        $this->controller = $route['controller'];
+        $this->model = $route['controller'];
+        $this->view = $route['action'];
+        $this->prefix = $route['prefix'];
+    }
+
+    public function set($data){
+        $this->data = $data;
+    }
+
+    public function setMeta($title = '', $desc = '', $keywords = ''){
+        $this->meta['title'] = $title;
+        $this->meta['desc'] = $desc;
+        $this->meta['keywords'] = $keywords;
     }
 
 }
